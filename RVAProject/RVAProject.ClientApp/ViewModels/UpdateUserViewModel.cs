9fdb7f19679cbeb1ef0c2a5f9abc3adf73943070
bbratic <bojana.bratic14@gmail.com>
2024-06-04 18:14:39 +0200

    Add initial views

---
+++ RVAProject/RVAProject.ClientApp/ViewModels/UpdateUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RVAProject.ClientApp.ViewModels
+{
+    internal class UpdateUserViewModel:BindableBase
+    {
+        public UpdateUserViewModel() {
+            Title = "Update User";
+        }
+    }
+}
